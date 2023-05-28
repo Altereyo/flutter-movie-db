e7f35c13580ec4542c0310ef9da2ec5af5b3ac9a
Altereyo <r8.lebz@gmail.com>
2023-05-28 17:56:22 +0300

    code: connection service

--- lib/data/services/connection_service.dart
+++ lib/data/services/connection_service.dart
@@ -5,18 +5,29 @@ import 'package:get/get.dart';
 class ConnectionService extends GetxService {
   StreamSubscription<ConnectivityResult>? connectionStreamSubscription;
 
+  @override
+  void onInit() {
+    super.onInit();
+    initConnectionListener();
+  }
+
+  Future<bool> isConnected () async {
+    final result = await Connectivity().checkConnectivity();
+    return (result == ConnectivityResult.mobile ||
+        result == ConnectivityResult.ethernet ||
+        result == ConnectivityResult.wifi ||
+        result == ConnectivityResult.vpn);
+  }
+
   Future<void> initConnectionListener() async {
     // we are not disposing listener anywhere, because it should work always, but we have method in service to do this
     connectionStreamSubscription = Connectivity().onConnectivityChanged.listen((ConnectivityResult result) async {
-      final bool connectionExists = (result == ConnectivityResult.mobile ||
-          result == ConnectivityResult.ethernet ||
-          result == ConnectivityResult.wifi ||
-          result == ConnectivityResult.vpn);
+      final bool isConnected = await this.isConnected();
 
-      if (connectionExists && Get.currentRoute == '/connection_error') {
-        // TODO go back from /connection_error
-      } else if (!connectionExists && Get.currentRoute != '/connection_error'){
-        // TODO go to /connection_error
+      if (isConnected && Get.currentRoute == '/connection_error') {
+        Get.offAllNamed('/home');
+      } else if (!isConnected && Get.currentRoute != '/connection_error'){
+        Get.toNamed('/connection_error');
       }
     });
   }
